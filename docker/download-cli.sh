f1f37b79903c9039db377dada0b03b965e4e601c
davidebianchi03 <dadebianchi2003@gmail.com>
2024-11-12 21:05:53 +0100

    build docker image in cicd

---
+++ docker/download-cli.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+REPO_OWNER=davidebianchi03
+REPO_NAME=codebox-cli
+LINUX_CLI_ARTIFACT_NAME=codebox-cli-linux-amd64
+WINDOWS_CLI_ARTIFACT_NAME=codebox-cli-windows-amd64.exe
+
+
+response=$(curl -s -H "Authorization: token $GH_TOKEN" "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/releases")
+releases_created_at=$(echo "$response" | jq '.[].created_at')
+sorted_releases=$(echo $releases_created_at | xargs -n1 | sort -r | xargs)
+latest_release_created_at=$(echo $sorted_releases | cut -d ' ' -f1)
+
+latest_release=$(echo "$response" | jq ".[] | select(.created_at == \"${latest_release_created_at}\")")
+linux_cli_asset_id=$(echo $latest_release | jq ".assets[] | select(.name == \"${LINUX_CLI_ARTIFACT_NAME}\") | .id")
+windows_cli_asset_id=$(echo $latest_release | jq ".assets[] | select(.name == \"${WINDOWS_CLI_ARTIFACT_NAME}\") | .id")
+
+curl -L -H "Authorization: token $GH_TOKEN" -H "Accept: application/octet-stream" "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/releases/assets/$linux_cli_asset_id" -o $LINUX_CLI_ARTIFACT_NAME
+curl -L -H "Authorization: token $GH_TOKEN" -H "Accept: application/octet-stream" "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/releases/assets/$windows_cli_asset_id" -o $WINDOWS_CLI_ARTIFACT_NAME
