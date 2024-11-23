dde02cada522954788ceae974ecc43105e32a5c3
davidebianchi03 <dadebianchi2003@gmail.com>
2024-11-23 11:21:27 +0100

    script to download cli - print details

--- docker/download-cli.sh
+++ docker/download-cli.sh
@@ -12,8 +12,13 @@ sorted_releases=$(echo $releases_created_at | xargs -n1 | sort -r | xargs)
 latest_release_created_at=$(echo $sorted_releases | cut -d ' ' -f1)
 
 latest_release=$(echo "$response" | jq ".[] | select(.created_at == \"${latest_release_created_at}\")")
+echo "CLI Latest Release: ${latest_release}"
+
 linux_cli_asset_id=$(echo $latest_release | jq ".assets[] | select(.name == \"${LINUX_CLI_ARTIFACT_NAME}\") | .id")
+echo "CLI Linux asset id: ${linux_cli_asset_id}"
+
 windows_cli_asset_id=$(echo $latest_release | jq ".assets[] | select(.name == \"${WINDOWS_CLI_ARTIFACT_NAME}\") | .id")
+echo "CLI Windows asset id: ${windows_cli_asset_id}"
 
 curl -L -H "Authorization: token $GH_TOKEN" -H "Accept: application/octet-stream" "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/releases/assets/$linux_cli_asset_id" -o $LINUX_CLI_ARTIFACT_NAME
 curl -L -H "Authorization: token $GH_TOKEN" -H "Accept: application/octet-stream" "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/releases/assets/$windows_cli_asset_id" -o $WINDOWS_CLI_ARTIFACT_NAME
