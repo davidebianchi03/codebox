b7db16d6c2befea22bbaf4258177a8ec7cae1cc4
Davide Bianchi <dadebianchi2003@gmail.com>
2025-03-09 17:28:09 +0000

    frontend - page 404

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,29 +1,52 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
-import LoginPage from './pages/Login';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/Login";
 import HomePage from "./pages/Home";
 import "@tabler/core/dist/css/tabler.min.css";
-import 'bootstrap/dist/js/bootstrap.js';
+import "bootstrap/dist/js/bootstrap.js";
 import AuthRequired from "./pages/AuthRequired";
 import CreateWorkspace from "./pages/CreateWorkspace";
 import WorkspaceDetails from "./pages/WorkspaceDetails";
 import Profile from "./pages/Profile";
-
+import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path='' element={<AuthRequired><HomePage /></AuthRequired>} />
-        <Route path='/create-workspace' element={<AuthRequired><CreateWorkspace /></AuthRequired>} />
-        <Route path='/workspaces/:id' element={<AuthRequired><WorkspaceDetails /></AuthRequired>} />
-        <Route path='/profile' element={<AuthRequired><Profile /></AuthRequired>} />
-      </Routes>
-      <Routes>
-        <Route path='login' element={<LoginPage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route
+          path=""
+          element={
+            <AuthRequired>
+              <HomePage />
+            </AuthRequired>
+          }
+        />
+        <Route
+          path="/create-workspace"
+          element={
+            <AuthRequired>
+              <CreateWorkspace />
+            </AuthRequired>
+          }
+        />
+        <Route
+          path="/workspaces/:id"
+          element={
+            <AuthRequired>
+              <WorkspaceDetails />
+            </AuthRequired>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <AuthRequired>
+              <Profile />
+            </AuthRequired>
+          }
+        />
+        <Route element={<NotFound />} path="*" />
       </Routes>
     </Router>
   );
